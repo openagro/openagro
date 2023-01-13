f872f9418b3364e9cce9aaf122f344a5e4d7f8c0
bluespada <pentingmain@gmail.com>
2023-01-13 16:36:56 +0800

    initialize license and basic cargo init

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+// Copyright (c) 2023 OpenAgro Developers
+//
+// This file under Lesser General Public License v3.0, please read accompanying file
+// copy or, read on https://www.gnu.org/licenses/lgpl-3.0.html
+
+fn main() {
+    println!("Hello, world!");
+}
