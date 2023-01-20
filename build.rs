d72db142e3b089bacd8d88417a9a016ed84e3ace
bluespada <pentingmain@gmail.com>
2023-01-20 16:40:50 +0800

    feat : prepare for extension data assets

---
+++ build.rs
@@ -0,0 +1,3 @@
+fn main(){
+    // std::process::Command::new("yarn").arg("build").spawn().expect("failed to spawn process");
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,19 @@
 /// This file under Lesser General Public License v3.0, please read accompanying file
 /// copy or, read on https://www.gnu.org/licenses/lgpl-3.0.html
 
+use openagro_extension::extension::ExtensionMetadata;
+use openagro_test_extension::metadata as test_extension;
+use openagro_extension_sales::metadata as sales_extension;
+
+use serde_json;
+
 fn main() {
+
+    let list_extension : Vec<ExtensionMetadata> = vec![test_extension(), sales_extension()];
+
+    for extensions in list_extension {
+        let json = serde_json::to_string_pretty(&extensions).unwrap();
+        println!("{}", json);
+    }
 
 }
